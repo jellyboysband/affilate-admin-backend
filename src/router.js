ee601cfcf08b460240473e6a424d902ce6602cd4
prilipko@novotorica.com <prilipko@novotorica.com>
2019-10-01 18:28:38 +0300

    q

--- src/router.js
+++ src/router.js
@@ -5,24 +5,23 @@ const config = require('./config');
 
 const router = new Router();
 
-router.post('/login', async ctx => {
-  const { password } = ctx.request.body();
+router.post('/admin/login', async ctx => {
+  console.log('TCL: ctx', ctx);
+  const { password } = ctx.request.body;
   if (password === config.adminPassword) {
     ctx.body = { token: jwt.sign({ timestamp: Date.now() }, config.jwtSalt, { expiresIn: config.jwtExpires }) };
   } else {
-    ctx.throw(403, 'Password Incorrect')
+    ctx.throw(403, 'Password Incorrect');
   }
-})
+});
 
 router.get('/', ctx => {
-  console.log('kek')
-})
+  console.log('kek');
+});
 
 // router.get('/product', authMiddleware, async ctx => {
-router.get('/product', async ctx => {
-
-  ctx.body = (await ctx.rabbit.get(config.getQ, { noAck: true })).content.toString()
-})
-
+router.get('/admin/product', async ctx => {
+  ctx.body = (await ctx.rabbit.get(config.getQ, { noAck: true })).content.toString();
+});
 
 module.exports = router.routes();
